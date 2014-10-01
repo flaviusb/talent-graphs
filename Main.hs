977128d26f01fa0ca4f30e21eb15aa3fba5041c5
Justin (:flaviusb) Marsh <justin.marsh@flaviusb.net>
2014-10-02 11:41:03 +1300

    Add in start of spell squares.

--- Main.hs
+++ Main.hs
@@ -2,19 +2,26 @@
 import Data.Yaml
 import Control.Applicative -- <$>, <*>
 import Data.Maybe (fromJust)
+import Data.List (intersperse)
 
 import qualified Data.ByteString.Char8 as BS
 
 
 data Ability = Ability {
     name :: String,
-    prerequisites :: [[String]]}
+    prerequisites :: [[String]],
+    level :: String,
+    xp :: Int,
+    description :: String}
     deriving (Show)
 
 instance FromJSON Ability where
     parseJSON (Object v) = Ability <$>
                            v .: "Name" <*>
-                           v .: "Prerequisites"
+                           v .: "Prerequisites" <*>
+                           v .: "Level" <*>
+                           v .: "XP" <*>
+                           v .: "Description"
     parseJSON _ = error "Can't parse ability from YAML"
 
 quoted :: String -> String
@@ -24,11 +31,31 @@ style :: String -> String
 style "black" = ""
 style colour  = " [color=" ++ colour ++ "]"
 
+arrows :: [Ability] -> String    
+arrows abilities =
+    let colours = ["black", "blue", "green", "orange"]
+    in foldl (\acc ability -> acc ++ foldl (\acc2 prereqlist -> acc2 ++ foldl (\acc3 prereq -> acc3 ++ (quoted $ fst prereq) ++ " -> " ++  (quoted $ name ability) ++ (style $ snd prereq) ++ ";\n") "" (map (\foo -> (foo, snd prereqlist)) $ fst prereqlist )) "" (zip (prerequisites ability) colours)) "" abilities
+
+prereq_to_span :: Ability -> String
+prereq_to_span ability = concat $ intersperse " *or* " $ map (\conj -> concat $ intersperse ", " conj) (prerequisites ability)
+
+ability_square :: Ability -> String
+ability_square ability = concat [
+              "*", name ability, "*\n",
+              "XP Cost: ", show (xp ability), " (", level ability, ")\n",
+              "_Prerequisites:_ ", prereq_to_span ability, "\n",
+              description ability, "\n"]
+
+ability_squares :: [Ability] -> String
+ability_squares abilities = "\n[cols=\"2\"]\n|===\n" ++ (concat (map (\ability -> "|\n" ++ (ability_square ability) ++ "\n") abilities)) ++ "|===\n"
+
+
 main = do
          ymlData <- BS.readFile "abilities.yml"
-         let colours = ["black", "blue", "green", "orange"]
          let abilities = fromJust (Data.Yaml.decode ymlData :: Maybe [Ability])
-         let start = "digraph G {\n"
-         let end  = "}"
-         let arrows = foldl (\acc ability -> acc ++ foldl (\acc2 prereqlist -> acc2 ++ foldl (\acc3 prereq -> acc3 ++ (quoted $ fst prereq) ++ " -> " ++  (quoted $ name ability) ++ (style $ snd prereq) ++ ";\n") "" (map (\foo -> (foo, snd prereqlist)) $ fst prereqlist )) "" (zip (prerequisites ability) colours)) "" abilities
-         putStrLn $ start ++ arrows ++ end
+         print abilities
+         frontmatter <- readFile "frontmatter.asciidoc.part"
+         let start = "\n[graphviz, foo, png]\n....\ndigraph G {\n"
+         let end  = "}\n....\n"
+         let diagram = arrows abilities
+         putStrLn $ frontmatter ++ (ability_squares abilities) ++ start ++ diagram ++ end
